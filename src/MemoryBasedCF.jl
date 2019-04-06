10b4dc931fe2165116c8669a19f782b8d577a18d
yubessy <yubessy0@gmail.com>
2019-04-06 15:32:40 +0900

    export

--- src/MemoryBasedCF.jl
+++ src/MemoryBasedCF.jl
@@ -1,3 +1,6 @@
 module MemoryBasedCF
     include("core.jl")
+    export memorize
+    export userbased_scores, itembased_scores
+    export userbased_recommendations, itembased_recommendations
 end
