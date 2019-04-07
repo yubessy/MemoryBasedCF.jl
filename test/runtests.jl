1570eb232b6c6e87051e22efe0cd8b2f2c40e1dc
yubessy <yubessy0@gmail.com>
2019-04-07 11:02:52 +0900

    fix item-based test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -42,22 +42,22 @@ R = sparse([
 
     @testset "itembased" begin
         expected_scores = [
-            2.25  0.75  1.50
-            0.75  1.50  2.25
-            1.50  0.75  2.25
-            2.00  2.00  2.00
+            2.0  1.0  1.5
+            1.0  1.5  2.0
+            1.5  1.0  2.0
+            2.0  2.0  2.0
         ]
         expected_ranked_items = [
-            1 3 2
-            3 2 1
-            3 1 2
-            1 2 3
+            1  3  2
+            3  2  1
+            3  1  2
+            1  2  3
         ]
         expected_ranked_scores = [
-            2.25  1.50  0.75
-            2.25  1.50  0.75
-            2.25  1.50  0.75
-            2.00  2.00  2.00
+            2.0  1.5  1.0
+            2.0  1.5  1.0
+            2.0  1.5  1.0
+            2.0  2.0  2.0
         ]
 
         @test isapprox(itembased_scores(m, [1, 2, 3, 4]), expected_scores, atol = 1e-2)
@@ -73,7 +73,7 @@ R = sparse([
 
         ranked_items, ranked_scores = itembased_rankings(m, 2, [1, 2], [1, 3])
         @test ranked_items == [1 3; 3 1]
-        @test isapprox(ranked_scores, [2.25  1.50; 2.25  0.75], atol = 1e-2)
+        @test isapprox(ranked_scores, [2.0  1.5; 2.0  1.0], atol = 1e-2)
     end
 
     @testset "userbased" begin
